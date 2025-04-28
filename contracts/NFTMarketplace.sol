13889c9c4fe84961cda8f2a77005407992701b12
om salunke <omsaunke343@gmail.com>
2025-04-28 12:55:01 +0530

    contract creation done

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -135,4 +135,47 @@ contract NFTMarketplace is ERC721URIStorage {
         }
         return items;
     }
+
+    //purchase item
+    function fetchMyNFT() public view returns (MarketItem[] memory) {
+        uint256 totalCount = _tokenIds.current();
+        uint256 itemCount = 0;
+        uint256 currentIndex = 0;
+        for (uint256 i = 0; i < totalCount; i++) {
+            if (idMarketItem[i + 1].owner == msg.sender) {
+                itemCount++;
+            }
+        }
+        MarketItem[] memory items = new MarketItem[](itemCount);
+        for (uint256 i = 0; i < totalCount; i++) {
+            if (idMarketItem[i + 1].owner == msg.sender) {
+                items[currentIndex] = idMarketItem[i + 1];
+                currentIndex++;
+            }
+        }
+        return items;
+    }
+
+    //single user item
+    function fetchItemsListed() public view returns(MarketItem[] memory){
+        uint256 totalCount = _tokenIds.current();
+        uint256 itemCount =0;
+        uint256 currentIndex = 0;
+
+        for(uint256 i=0;i<totalCount;i++){
+            if(idMarketItem[i+1].seller==msg.sender){
+                itemCount++;
+            }
+        }
+        MarketItem[] memory items = new MarketItem[](itemCount);
+        for(uint256 i=0;i<totalCount;i++){
+            if(idMarketItem[i+1].seller==msg.sender){
+                uint256 currentId = i+1;
+                MarketItem storage currentItem = idMarketItem[currentId];
+                items[currentIndex]=currentItem;
+                currentIndex++;
+            }
+        }
+        return items;
+    }
 }
